8709130add40f79b2254f2cffc983647d9bf7ed0
GalaletsangM <preciousmafoko@gmail.com>
2024-05-21 21:11:25 -0500

    updated import axios line

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import axios from "axios";
+import axios from "./lib/axios";
 import "./App.css";
 import { Container, Row, Col } from "react-bootstrap";
 import "./../node_modules/bootstrap/dist/css/bootstrap.min.css";
@@ -9,7 +9,7 @@ export default function Weather() {
   //let [loaded, setLoaded] = useState(true);
   const [weather, setWeather] = useState({});
   const [heading, setHeading] = useState(`${city}`);
-  const [date, setDate] = useState("Wednesday, 21:48")
+  const [date, setDate] = useState("Wednesday, 21:48");
 
   function displayWeather(response) {
     console.log(response.data.main);
@@ -20,7 +20,7 @@ export default function Weather() {
       icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
       description: response.data.weather[0].description,
     });
-    //let setDate = new Date(response.data.time * 1000);
+    
   }
 
   function handleSubmit(event) {
@@ -29,7 +29,6 @@ export default function Weather() {
     let apiKey = "99b8f9330a1bfba3a85e523fd3c2e528";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(displayWeather);
-    
   }
 
   function updateCity(event) {
@@ -45,50 +44,47 @@ export default function Weather() {
     </form>
   );
 
-  
   return (
     <div className="app">
       <div className="weatherContent">
         <div className="form">{form}</div>
-        <Container>
-          <Row>
-            <Col>
-              <h1 className="heading">{heading}</h1>
-              <ul className="list">
-                <li>
-                  <span></span> Wednesday, 21:48.{" "}
-                  <span>{weather.description}</span>
-                </li>
-                <li>
-                  Humidity: <strong>{weather.humidity}</strong>%, Wind{" "}
-                  <strong>{weather.wind}</strong>km/h
-                </li>
-              </ul>
-            </Col>
-            <Col>
-              <div className="temperature-container">
-                <span>
-                  <img src={weather.icon} alt={weather.description} />
-                </span>
-                <span className="temp">{Math.round(weather.temperature)}</span>
-                <span className="unit">°C</span>
-              </div>
-            </Col>
-          </Row>
-        </Container>
-        <Container className="weatherForecast">
-          <Row>
-            <Col>
-              <p>Tue</p>
-              
-            </Col>
-            <Col><p>Wed</p></Col>
-            <Col><p>Thu</p></Col>
-            <Col><p>Fri</p></Col>
-            <Col><p>Sat</p></Col>
-            <Col><p>Sun</p></Col>
-          </Row>
-        </Container>
+        <main>
+          <Container>
+            <Row>
+              <Col>
+                <h1 className="heading">{heading}</h1>
+                <ul className="list">
+                  <li>
+                    <span>Wednesday, 21:48 </span>
+                    <span>{weather.description}</span>
+                  </li>
+                  <li>
+                    Humidity: <strong>{weather.humidity}</strong>%, Wind{" "}
+                    <strong>{weather.wind}</strong>km/h
+                  </li>
+                </ul>
+              </Col>
+              <Col>
+                <div className="temperature-container">
+                  <span>
+                    <img src={weather.icon} alt={weather.description} />
+                  </span>
+                  <span className="temp">
+                    {Math.round(weather.temperature)}
+                  </span>
+                  <span className="unit">°C</span>
+                </div>
+              </Col>
+            </Row>
+          </Container>
+          <Container className="weatherForecast"></Container>
+        </main>
+        <footer>
+          This project was coded by
+          <a href="https://github.com/GalaletsangM/"> Galaletsang</a> , is
+          <a href="https://github.com/GalaletsangM/weather-react"> open sources on Github </a>
+          and <a href=""></a>
+        </footer>
       </div>
     </div>
   );
